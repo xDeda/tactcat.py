3c2ca0640347ca6ef833d330c28cae6033b2ae9b
Niclas <xDeda@users.noreply.github.com>
2018-09-09 18:01:52 +0200

    Update drawingapp.py
    Made some crucial updates to where it draws in relation to your cursor and the window, it'll draw from the coordinates of the upper left of the window now and not from where your cursor is on your screen (as much)

--- drawingapp.py
+++ drawingapp.py
@@ -12,12 +12,14 @@ phase = 1
 togglevar = 0
 
 def callback(e):
-	global fromx, fromy, tox, toy, phase
+	global fromx, fromy, tox, toy, phase, infox, infoy
 	pos = pyautogui.position()
+	pos[0]-infoz[0]
+	pos[1]-infoz[1]
+	infoz = click()
 	if (phase == 0):
 		tox = pos[0]
-		toy = pos[1]
-		w.create_line((fromx-239)-master.winfo_x(),(fromy-31)-master.winfo_y(),(tox-239)-master.winfo_x(),(toy-31)-master.winfo_y(), fill="Black")
+		w.create_line(fromx, fromy, tox, toy, fill="Black")
 		fromx = pos[0]
 		fromy = pos[1]
 		draw.append([fromx,fromy])
@@ -32,43 +34,13 @@ def callback(e):
 
 def split(q):
 	global fromx, fromy
+	infoz = click()
 	pos = pyautogui.position()
 	draw.append(5000)
-	fromx = pos[0]
-	fromy = pos[1]
+	fromx = pos[0]-infoz[0]
+	fromy = pos[1]-infoz[1]
 	draw.append([fromx,fromy])
 	print("5000")
-
-def getArray(drawing):
-	n = 0
-	i = len(drawing)
-	print(len(drawing))
-	
-	thethingX = drawing[0][0]
-	thethingY = drawing[0][1]
-	
-	while (n<i):
-		if (drawing[n] != 5000):
-			if (drawing[n][0] < thethingX):
-				thethingX = drawing[n][0]
-				print(thethingX, "is the new X low")
-			if (drawing[n][1] < thethingY):
-				thethingY = drawing[n][1]
-				print(",",thethingY, " is the new Y low")
-		n = n+1
-	
-	n = 0
-	
-	while (n<i):
-		if (drawing[n] != 5000):
-			drawing[n][0] = drawing[n][0] - thethingX
-			drawing[n][1] = drawing[n][1] - thethingY
-			# print(array[n], ",")
-		# elif (array[n] == 5000):
-			# print("5000,")
-		n = n+1
-
-	return drawing
 
 
 def getDraw():
@@ -79,16 +51,14 @@ def getDraw():
 	draw = eval(drawing)
 	drawing = draw
 
-	drawing = getArray(drawing)
-
 	c = 0
 	i = len(drawing)
-	w.create_line(drawing[c][0]+50,drawing[c][1]+50,drawing[c+1][0]+50,drawing[c+1][1]+50, fill="Black")
+	w.create_line(drawing[c][0],drawing[c][1],drawing[c+1][0],drawing[c+1][1], fill="Black")
 	while (c+1<i):
 		if(drawing[c] == 5000 or drawing[c+1] == 5000):
 			c += 1
 		else:
-			w.create_line(drawing[c][0]+50,drawing[c][1]+50,drawing[c+1][0]+50,drawing[c+1][1]+50, fill="Black")
+			w.create_line(drawing[c][0],drawing[c][1],drawing[c+1][0],drawing[c+1][1], fill="Black")
 			c += 1
 	return;
 
@@ -96,8 +66,7 @@ def update():
 	global draw
 	T.configure(state='normal')
 	T.delete('1.0', END)
-	drawz = getArray(draw)
-	drawz = str(drawz)[1:-1]
+	drawz = str(draw)[1:-1]
 	T.insert(INSERT, drawz)
 	T.configure(state='disabled')
 	w.delete("all")
@@ -153,6 +122,11 @@ def focus_next_window(event):
     event.widget.tk_focusNext().focus()
     return("break")
 
+def click():
+	infox = master.winfo_x()+238
+	infoy = master.winfo_y()+31
+	return infox, infoy
+
 master = Tk()
 master.wait_visibility(master)
 master.title('Drawing App')
@@ -162,7 +136,7 @@ master.bind('q', split)
 master.bind('z', play)
 w = Canvas(master, bg="White")
 w.pack(side=RIGHT, fill=BOTH, expand=True)
-okButton = Button(master, text="Record", command=callback)
+okButton = Button(master, text="Record", command=callback, width=30)
 okButton.pack(expand=False, fill=X)
 splitButton = Button(master, text="Split", command=split)
 splitButton.pack(expand=False, fill=X)
@@ -182,6 +156,6 @@ yscrollbar.pack(side=RIGHT, fill=Y)
 T = Text(master, width=30, height=10, state='normal', font=("Source Code Pro", 8), yscrollcommand=yscrollbar.set)
 T.bind("<Tab>", focus_next_window)
 T.pack(expand=True, fill=Y)
-yscrollbar.config( command = T.yview )
+yscrollbar.config(command = T.yview)
 
 mainloop()
